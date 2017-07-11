c4cd28c724741583335f0bb68aa3421183c93488
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-07-11 22:50:49 +0200

    WIP: 'Module is not available'

--- src/app/hello.js
+++ src/app/hello.js
@@ -3,53 +3,5 @@ angular
   .component('app', {
     templateUrl: 'app/hello.html',
     controller: function () {
-      this.users = [
-        {
-          id: 1,
-          username: 'test',
-          password: 'password'
-        },
-        {
-          id: 2,
-          username: 'test2',
-          password: 'password'
-        }
-      ];
-
-      this.user = {};
-
-      this.editing = false;
-
-      this.nextId = 3;
-
-      this.elementContent = 'add';
-
-      this.addUser = function () {
-        if (this.editing) {
-          var index = this.users.indexOf(this.user);
-          if (index !== -1) {
-            this.users[index] = this.user;
-            this.editing = false;
-          }
-        } else {
-          this.user.id = this.nextId++;
-          this.users.push(this.user);
-        }
-        this.elementContent = 'add';
-        this.user = {};
-      };
-
-      this.editUser = function (user) {
-        this.editing = true;
-        this.elementContent = 'edit';
-        this.user = user;
-      };
-
-      this.deleteUser = function (user) {
-        var index = this.users.indexOf(user);
-        if (index !== -1) {
-          this.users.splice(index, 1);
-        }
-      };
     }
   });
--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,9 @@ function routesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
     .state('app', {
       url: '/',
       component: 'app'
+    })
+    .state('users', {
+      url: '/users',
+      component: 'userList'
     });
 }
